1bbd64d7e305864935e0745e181b92382846c2f5
awesomeankit <ankitsawant40@gmail.com>
2022-04-06 12:02:38 +0530

    Initial commit

---
+++ src/main/java/com/hkechallenge/psudoqueue/PsudoqueueApplication.java
@@ -0,0 +1,13 @@
+package com.hkechallenge.psudoqueue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsudoqueueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsudoqueueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hkechallenge/psudoqueue/PsudoqueueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hkechallenge.psudoqueue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PsudoqueueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
